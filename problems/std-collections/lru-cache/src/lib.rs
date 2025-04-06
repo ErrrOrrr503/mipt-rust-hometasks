227917ee136b98d7995681898c34c862cb871ad7
errrorrr503 <egortit-33@yandex.ru>
2025-04-07 01:48:24 +0300

    Added LRU task.

--- problems/std-collections/lru-cache/src/lib.rs
+++ problems/std-collections/lru-cache/src/lib.rs
@@ -5,22 +5,59 @@ use std::hash::Hash;
 
 #[derive(Debug)]
 pub struct LRUCache<K, V> {
-    // TODO: your code goes here.
+    cur_time: usize,
+    len: usize,
+    capacity: usize,
+    map: HashMap<K, (V, usize)>,
+    usage_times: BTreeMap<usize, K>,
 }
 
 impl<K: Clone + Hash + Ord, V> LRUCache<K, V> {
     pub fn new(capacity: usize) -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        assert!(capacity > 0, "Capacity must be positive");
+        Self {
+            cur_time: 0,
+            len: 0,
+            capacity: capacity,
+            map: HashMap::with_capacity(capacity),
+            usage_times: BTreeMap::new(),
+        }
     }
 
     pub fn get(&mut self, key: &K) -> Option<&V> {
-        // TODO: your code goes here.
-        unimplemented!()
+        if let Some(entry) = self.map.get_mut(&key) {
+            self.cur_time += 1;
+            let old_time = std::mem::replace(&mut entry.1, self.cur_time);
+            self.usage_times.remove(&old_time);
+            self.usage_times.insert(self.cur_time, key.clone());
+            Some(&entry.0)
+        } else {
+            None
+        }
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.cur_time += 1;
+        if let Some(curr_entry) = self.map.get_mut(&key) {
+            /* old entry updated */
+            let old_val = std::mem::replace(&mut curr_entry.0, value);
+            let old_time = std::mem::replace(&mut curr_entry.1, self.cur_time);
+            self.usage_times.remove(&old_time);
+            self.usage_times.insert(self.cur_time, key);
+            Some(old_val)
+        } else {
+            /* new entry inserted  */
+            self.map.insert(key.clone(), (value, self.cur_time));
+            if self.len == self.capacity {
+                let (_, lru_key) = self.usage_times.pop_first().unwrap();
+                self.usage_times.insert(self.cur_time, key);
+                self.map.remove(&lru_key);
+                None
+            } else {
+                self.len += 1;
+                self.usage_times.insert(self.cur_time, key);
+                None
+            }
+        }
     }
 }
